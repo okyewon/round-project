0db9c91e61ba7b47df569d43d7bbd58b77f0d24a
okwonye@gmail.com <okwonye@gmail.com>
2024-05-16 15:28:34 +0900

    로그아웃 confirm 후 로그인페이지로 이동

--- src/components/routes/Mypage.tsx
+++ src/components/routes/Mypage.tsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router";
 import { auth } from "../../firebase";
 
 const Mypage = () => {
-  const logOut = () => {
-    auth.signOut();
+  const navigate = useNavigate();
+  const logOut = async () => {
+    const ok = confirm("로그아웃 하시겠어요?");
+    if (ok) {
+      await auth.signOut();
+      navigate("/login");
+    }
   };
 
   return (
